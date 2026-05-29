5e3b9b11ba3c7fcf0049678ab22dc7a150ca1b13
HediAyarii <hediayari.sascode@gmail.com>
2026-05-29 18:03:37 +0100

    version isseu

---
+++ mobile/android/app/src/main/kotlin/com/taprivo/taprivo_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.taprivo.taprivo_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:taprivo_mobile/widgets/brand.dart';
+
+void main() {
+  test('hexColor parses #RRGGBB brand colours', () {
+    expect(hexColor('#D85A30'), const Color(0xFFD85A30));
+    expect(hexColor('04342C'), const Color(0xFF04342C));
+  });
+}
